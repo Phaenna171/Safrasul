cc7a43d70dca12c511d9a95b2264013a6e1d7b17
aenamartinelli <62079043+Aenewsss@users.noreply.github.com>
2025-05-21 18:07:45 -0300

    ftr

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -86,6 +86,17 @@ export default function Header({ footerRef }) {
             >
               Contato
             </Link>
+            <Link
+              href="https://www.integracaosementes.com.br/ftr"
+              className={`text-lg leading-6 text-[#9B9B9B] font-openSans font-medium ${activeLink === "contact" ? "border-b-4 border-[#F17708] text-customGreen" : ""
+                }`}
+              onClick={(e) => {
+                e.preventDefault(); // Prevent default link behavior
+                scrollToFooter();
+              }}
+            >
+              Suporte Técnico
+            </Link>
           </PopoverGroup>
         </nav>
         <Dialog
